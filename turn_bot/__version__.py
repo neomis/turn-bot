f64a831b328c5eb6d058be0aca84a7ae56e9edf1
Justin Blair <justin.blair@broadcom.com>
2019-05-23 10:23:18 -0600

    Added github url

--- turn_bot/__version__.py
+++ turn_bot/__version__.py
@@ -1,7 +1,7 @@
 """Package Meta Data."""
 __title__ = 'turn_bot'
 __description__ = 'CIV6  Turn bot for Discord.'
-__url__ = ''
+__url__ = 'https://github.com/neomis/turn-bot'
 __version__ = "1.0.0"
 __author__ = 'Justin Blair'
 __author_email__ = 'neomis@gmail.com'
