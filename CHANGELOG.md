69eb0f37088542b2c2be27d0c8b7e69c2c676401
Justin Blair <justin.blair@broadcom.com>
2019-05-23 10:28:14 -0600

    Ready for 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # CHANGELOG
 
+## 1.0.1 (2019-05-23)
+* Cleanup of things I shouldn't have submitted to gitlab.
+* Added Repo URL.
+
 ## 1.0.0 (2019-05-22)
 * Program Created
--- turn_bot/__version__.py
+++ turn_bot/__version__.py
@@ -2,7 +2,7 @@
 __title__ = 'turn_bot'
 __description__ = 'CIV6  Turn bot for Discord.'
 __url__ = 'https://github.com/neomis/turn-bot'
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = 'Justin Blair'
 __author_email__ = 'neomis@gmail.com'
 __license__ = 'MIT'
