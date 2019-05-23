cf2b39e3624365147b8123341a775c2f42b03ab0
Justin Blair <justin.blair@broadcom.com>
2019-05-23 10:17:24 -0600

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Turn Bot Python Program.
+
+
+Instructions.
+
+git clone https://github.com/neomis/turn-bot.git
+cd turn-bot
+virtualenv venv
+source venv/bin/activate
+pip install .
+turn_bot
+
+**Make sure you update turn_bot/config_local.py
--- turn_bot/config_local.py
+++ turn_bot/config_local.py
@@ -1,11 +1,8 @@
 DEBUG_LEVEL = 'ERROR'
-WEBHOOK_TOKEN = 'civ6'
-DISCORD_SERVER = "https://discordapp.com/api/webhooks/580920367854649345/_bXXmO9DgyTKWSvmhx6OghXK3L71EsyBqhoZekRkFrRNf7uMlYfrUW2Gk5OHfMs3qUpG"
-URL_PREFIX = '/civ6'
+WEBHOOK_TOKEN = ''
+DISCORD_SERVER = ""
+URL_PREFIX = ''
 HOST = '127.0.0.1'
 PORT = 9999
 USER_LIST = {
-    'neomis': 'neomis#0368',
-    'Kevoun': 'Kevoun#4154',
-    'Nesaru': 'roo#3225',
-    'Chance_the_Wrapper': 'Zachariah.Jebidiah#5238'}
+    'steam_name': 'discord_name#0368'}
